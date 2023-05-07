aafe3dec614859deaa96aa116ce563eccad6b82f
Radhika <rvramayra1998@gmail.com>
2023-05-07 09:23:16 +0530

    create dynamic style

---
+++ second_day/lib/BodyStyled.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+
+// class BodyStyled extends StatelessWidget {
+//   const BodyStyled({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Container(
+//           decoration: const BoxDecoration(
+//               gradient: LinearGradient(
+//             colors: [
+//               Color(0xFFCBC4C4),
+//               Color.fromARGB(225, 45, 7, 98),
+//               Color.fromARGB(255, 47, 5, 20)
+//             ],
+//             begin: Alignment.topCenter,
+//             end: Alignment.bottomRight,
+//           )),
+//         )
+//   }
+// }
--- second_day/lib/HomePage.dart
+++ second_day/lib/HomePage.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:second_day/StyledText.dart';
+
+const  startAlignment = Alignment.topCenter;
+const endAlignment = Alignment.bottomRight;
 
 class HomePage extends StatelessWidget {
   final int day = 3;
   final String name = "HelloMoto";
-  @override
+
+  @override // metadata.......
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         // backgroundColor:Colors.blue,
-        backgroundColor:Color.fromARGB(255,47,5,120),
+        backgroundColor: Color.fromARGB(255, 47, 5, 120),
         drawerScrimColor: Colors.black45,
         body: Container(
           decoration: const BoxDecoration(
-            gradient:LinearGradient(
-              colors: [Color(0xFFCBC4C4),
-              Color.fromARGB(225, 45, 7, 98)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              )
-               ),
-          child:  const Center(
-            child:   Text("welmcom To Child ",
-              style:  TextStyle(
-              color: Colors.white,
-              fontSize: 30
-              ),
-            ),
-            ),
+              gradient: LinearGradient(
+            colors: [
+              Color(0xFFCBC4C4),
+              Color.fromARGB(225, 45, 7, 98),
+              Color.fromARGB(255, 47, 5, 20)
+            ],
+            begin: startAlignment,
+            end: endAlignment,
+          )),
+          child: const Center(child: StyledText()),
         ),
       ),
     );
   }
 }
+
+
+
+
+// contructor ...
+// GardientContainer({key}):super(key:key) 
+// ALTERNATE METHOD 
+// GardientContainer({super.key});
